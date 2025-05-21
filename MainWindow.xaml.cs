86a36d8384bd7d5eb67fdbb2079e10dbd08373e6
Afonso-bc <2071523@student.uma.pt>
2025-05-21 18:44:02 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Classe_Carro_Datagrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Carro> osMeuscarros; 
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            osMeuscarros = new List<Carro>();
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Carro novo = new Carro();
+
+            novo.Cilindrada = int.Parse(textbox2.Text);
+            novo.Marca = textbox1.Text;
+
+            osMeuscarros.Add(novo);
+            dataGrid1.ItemsSource = null;
+            dataGrid1.ItemsSource = osMeuscarros;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGrid1.ItemsSource = osMeuscarros;
+        }
+    }
+}
---
+++ RelatorioDespesas.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Navegaçao_páginas
+{
+    /// <summary>
+    /// Interaction logic for RelatorioDespesas.xaml
+    /// </summary>
+    public partial class RelatorioDespesas : Page
+    {
+        public RelatorioDespesas()
+        {
+            InitializeComponent();
+        }
+        // Construtor personalizado para exibir as despesas
+        public RelatorioDespesas(object data) : this()
+        {
+            // Vincular a dados de relatórios de despesas
+            this.DataContext = data;
+
+        }
+    }
+}
